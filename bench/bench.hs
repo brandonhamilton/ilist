6f8b6b6224b047b7d02e5609e5d8750f5661f25c
Artyom <yom@artyom.me>
2016-05-14 20:45:43 +0300

    ifoldr, ifoldr1, ifilter, ifind

--- bench/bench.hs
+++ bench/bench.hs
@@ -1,5 +1,12 @@
+{-# LANGUAGE
+BangPatterns,
+MagicHash
+  #-}
+
+
 import Data.List
 import Data.List.Index
+import GHC.Exts
 
 import Criterion
 import Criterion.Main
@@ -7,18 +14,86 @@ import Criterion.Main
 
 main :: IO ()
 main = defaultMain [
+  bgroup "ifilter" [
+      bench "rec" $ nf (\n -> ifilter_rec (\i x -> rem (i+x) 5000 == 0) [0..n]) 100000,
+      bench "fold" $ nf (\n -> ifilter_fold (\i x -> rem (i+x) 5000 == 0) [0..n]) 100000,
+      bench "zip" $ nf (\n -> ifilter_zip (\i x -> rem (i+x) 5000 == 0) [0..n]) 100000,
+      bench "our" $ nf (\n -> ifilter (\i x -> rem (i+x) 5000 == 0) [0..n]) 100000 ],
+
+  bgroup "ifindIndices" [
+      bench "rec" $ nf (\n -> ifindIndices_rec (\i x -> rem (i+x) 5000 == 0) [0..n]) 100000,
+      bench "fold" $ nf (\n -> ifindIndices_fold (\i x -> rem (i+x) 5000 == 0) [0..n]) 100000,
+      bench "zip" $ nf (\n -> ifindIndices_zip (\i x -> rem (i+x) 5000 == 0) [0..n]) 100000,
+      bench "our" $ nf (\n -> ifindIndices (\i x -> rem (i+x) 5000 == 0) [0..n]) 100000 ],
+
+  bgroup "ifoldr" [
+      bench "zip" $ nf (\n -> ifoldr_zip (\i a x -> if rem x 16 == 0 then a+3*i else a+x) 0 [0..n]) 100000,
+      bench "our" $ nf (\n -> ifoldr (\i a x -> if rem x 16 == 0 then a+3*i else a+x) 0 [0..n]) 100000 ],
+
+  bgroup "ifoldr1" [
+      bench "zip" $ nf (\n -> ifoldr1_zip (\i a x -> if rem x 16 == 0 then a+3*i else a+x) [0..n]) 100000,
+      bench "our" $ nf (\n -> ifoldr1 (\i a x -> if rem x 16 == 0 then a+3*i else a+x) [0..n]) 100000 ],
+
   bgroup "ifoldl" [
-      bench "zip" $ nf (\n -> ifoldl_zip (\i a x -> if rem x 16 == 0 then a+3*i else a+x) 0 [0..n]) 100000,
-      bench "our" $ nf (\n -> ifoldl (\i a x -> if rem x 16 == 0 then a+3*i else a+x) 0 [0..n]) 100000 ],
+      bench "zip" $ nf (\n -> ifoldl_zip (\a i x -> if rem x 16 == 0 then a+3*i else a+x) 0 [0..n]) 100000,
+      bench "fold" $ nf (\n -> ifoldl_fold (\a i x -> if rem x 16 == 0 then a+3*i else a+x) 0 [0..n]) 100000,
+      bench "our" $ nf (\n -> ifoldl (\a i x -> if rem x 16 == 0 then a+3*i else a+x) 0 [0..n]) 100000 ],
 
   bgroup "ifoldl'" [
-      bench "zip" $ nf (\n -> ifoldl'_zip (\i a x -> if rem x 16 == 0 then a+3*i else a+x) 0 [0..n]) 100000,
-      bench "our" $ nf (\n -> ifoldl' (\i a x -> if rem x 16 == 0 then a+3*i else a+x) 0 [0..n]) 100000 ] ]
+      bench "zip" $ nf (\n -> ifoldl'_zip (\a i x -> if rem x 16 == 0 then a+3*i else a+x) 0 [0..n]) 100000,
+      bench "fold" $ nf (\n -> ifoldl'_fold (\a i x -> if rem x 16 == 0 then a+3*i else a+x) 0 [0..n]) 100000,
+      bench "our" $ nf (\n -> ifoldl' (\a i x -> if rem x 16 == 0 then a+3*i else a+x) 0 [0..n]) 100000 ] ]
 
+ifoldr_zip :: (Int -> a -> b -> b) -> b -> [a] -> b
+ifoldr_zip f a xs = foldr (\(i, x) acc -> f i x acc) a (zip [0..] xs)
+{-# INLINE ifoldr_zip #-}
+
+ifoldr1_zip :: (Int -> a -> a -> a) -> [a] -> a
+ifoldr1_zip f xs = snd (foldr1 (\(i, x) (j, y) -> (j, f i x y)) (zip [0..] xs))
+{-# INLINE ifoldr1_zip #-}
+
+ifoldl_zip :: (b -> Int -> a -> b) -> b -> [a] -> b
+ifoldl_zip f a xs = foldl (\acc (!i, x) -> f acc i x) a (zip [0..] xs)
 {-# INLINE ifoldl_zip #-}
-ifoldl_zip :: (Int -> b -> el -> b) -> b -> [el] -> b
-ifoldl_zip f a xs = foldl (\acc (i, x) -> f i acc x) a (zip [0..] xs)
 
+ifoldl'_zip :: (b -> Int -> a -> b) -> b -> [a] -> b
+ifoldl'_zip f a xs = foldl' (\acc (!i, x) -> f acc i x) a (zip [0..] xs)
 {-# INLINE ifoldl'_zip #-}
-ifoldl'_zip :: (Int -> b -> el -> b) -> b -> [el] -> b
-ifoldl'_zip f a xs = foldl' (\acc (i, x) -> f i acc x) a (zip [0..] xs)
+
+ifoldl_fold :: (b -> Int -> a -> b) -> b -> [a] -> b
+ifoldl_fold f z xs = foldl (\g x !i -> f (g (i-1)) i x) (const z) xs (length xs - 1)
+{-# INLINE ifoldl_fold #-}
+
+ifoldl'_fold :: (b -> Int -> a -> b) -> b -> [a] -> b
+ifoldl'_fold f z xs = foldl' (\g x !i -> f (g (i - 1)) i x) (const z) xs (length xs - 1)
+{-# INLINE ifoldl'_fold #-}
+
+ifilter_rec :: (Int -> a -> Bool) -> [a] -> [a]
+ifilter_rec p = go 0#
+  where
+    go _ [] = []
+    go i (x:xs) | p (I# i) x = x : go (i +# 1#) xs
+                | otherwise = go (i +# 1#) xs
+
+ifilter_fold :: (Int -> a -> Bool) -> [a] -> [a]
+ifilter_fold p = ifoldr (\i x xs -> if p i x then x : xs else xs) []
+{-# INLINE ifilter_fold #-}
+
+ifilter_zip :: (Int -> a -> Bool) -> [a] -> [a]
+ifilter_zip p xs = map snd (filter (uncurry p) (zip [0..] xs))
+{-# INLINE ifilter_zip #-}
+
+ifindIndices_rec :: (Int -> a -> Bool) -> [a] -> [Int]
+ifindIndices_rec p = go 0#
+  where
+    go _ [] = []
+    go i (x:xs) | p (I# i) x = I# i : go (i +# 1#) xs
+                | otherwise  = go (i +# 1#) xs
+
+ifindIndices_fold :: (Int -> a -> Bool) -> [a] -> [Int]
+ifindIndices_fold p = ifoldr (\i x xs -> if p i x then i : xs else xs) []
+{-# INLINE ifindIndices_fold #-}
+
+ifindIndices_zip :: (Int -> a -> Bool) -> [a] -> [Int]
+ifindIndices_zip p xs = map fst (filter (uncurry p) (zip [0..] xs))
+{-# INLINE ifindIndices_zip #-}
--- lib/Data/List/Index.hs
+++ lib/Data/List/Index.hs
@@ -9,10 +9,14 @@ BangPatterns
 module Data.List.Index
 (
   -- * Folds
+  ifoldr,
+  ifoldr1,
   ifoldl, ifoldl',
   ifoldl1, ifoldl1',
 
   -- * Search
+  ifilter,
+  ifind,
   ifindIndex,
   ifindIndices,
 )
@@ -40,8 +44,7 @@ ifoldlM
 imapAccumR
 imapAccumL
 
-ifind
-ifilter
+ipartition
 
 itraverse
 itraverse_
@@ -51,10 +54,6 @@ imapM
 imapM_
 iforM
 iforM_
-
-ifoldr
-ifoldr'
-ifoldr1
 
 izipWith
 izipWith3
@@ -66,31 +65,53 @@ izipWithM_
 -}
 
 
-ifoldl :: forall a b. (Int -> b -> a -> b) -> b -> [a] -> b
+-- Using unboxed ints here doesn't seem to result in any benefit
+ifoldr :: (Int -> a -> b -> b) -> b -> [a] -> b
+ifoldr f z xs = foldr (\x g i -> f i x (g (i+1))) (const z) xs 0
+{-# INLINE ifoldr #-}
+
+ifoldr1 :: (Int -> a -> a -> a) -> [a] -> a
+ifoldr1 f = go 0#
+  where go _ [x]    = x
+        go i (x:xs) = f (I# i) x (go (i +# 1#) xs)
+        go _ []     = errorEmptyList "ifoldr1"
+{-# INLINE [0] ifoldr1 #-}
+
+ifoldl :: forall a b. (b -> Int -> a -> b) -> b -> [a] -> b
 ifoldl k z0 xs =
   foldr (\(v::a) (fn :: (Int, b) -> b) ->
-          ONE_SHOT (\((!i)::Int, z::b) -> fn (i+1, k i z v)))
+          ONE_SHOT (\((!i)::Int, z::b) -> fn (i+1, k z i v)))
                    (snd :: (Int, b) -> b)
                    xs
                    (0, z0)
 {-# INLINE ifoldl #-}
 
-ifoldl' :: forall a b . (Int -> b -> a -> b) -> b -> [a] -> b
+ifoldl' :: forall a b. (b -> Int -> a -> b) -> b -> [a] -> b
 ifoldl' k z0 xs =
   foldr (\(v::a) (fn :: (Int, b) -> b) ->
-          ONE_SHOT (\((!i)::Int, z::b) -> z `seq` fn (i+1, k i z v)))
+          ONE_SHOT (\((!i)::Int, z::b) -> z `seq` fn (i+1, k z i v)))
                    (snd :: (Int, b) -> b)
                    xs
                    (0, z0)
 {-# INLINE ifoldl' #-}
 
-ifoldl1 :: (Int -> a -> a -> a) -> [a] -> a
+ifoldl1 :: (a -> Int -> a -> a) -> [a] -> a
 ifoldl1 f (x:xs) = ifoldl f x xs
 ifoldl1 _ []     = errorEmptyList "ifoldl1"
 
-ifoldl1' :: (Int -> a -> a -> a) -> [a] -> a
+ifoldl1' :: (a -> Int -> a -> a) -> [a] -> a
 ifoldl1' f (x:xs) = ifoldl' f x xs
 ifoldl1' _ []     = errorEmptyList "ifoldl1'"
+
+ifilter :: (Int -> a -> Bool) -> [a] -> [a]
+ifilter p ls = build $ \c n ->
+  let go x r k | p (I# k) x = x `c` r (k +# 1#)
+               | otherwise  = r (k +# 1#)
+  in foldr go (\_ -> n) ls 0#
+{-# INLINE ifilter #-}
+
+ifind :: (Int -> a -> Bool) -> [a] -> Maybe a
+ifind p = listToMaybe . ifilter p
 
 ifindIndex :: (Int -> a -> Bool) -> [a] -> Maybe Int
 ifindIndex p = listToMaybe . ifindIndices p
